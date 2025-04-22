5a1e90c27f02818b4c7b0e4657340853b644c2d9
Ульяна Аст <115323860+ulyanaast@users.noreply.github.com>
2025-04-23 02:08:19 +0300

    app.py

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ CORS(app)
 
 # Конфигурация
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLDER = os.path.join(BASE_DIR, '../frontend/static/uploads')
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static/uploads')
 DB_PATH = os.path.join(BASE_DIR, 'orders.db')
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
